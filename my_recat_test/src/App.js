e45876097a7cc85c53039aac8d1e8b3201b87af3
ponpanB <151675683+Bponpan@users.noreply.github.com>
2024-01-30 12:51:24 +0700

    up

--- my_recat_test/src/App.js
+++ my_recat_test/src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
 
-export default App;
+class evennums extends React.Component {
+  render() {
+    const evennums = [];
+    for (let i = 2; i <= 100; i += 2) {
+      evennums.push(i);
+    }
+    const formattedoutput = evennums.join(" , ");
+  
+  return (
+    <div className='component'>
+      <h1 className='text'>แสดงเลขคู่ระหว่าง 1 - 100</h1>
+      <p className='num'>{formattedoutput}</p>
+
+    </div>
+  )
+}
+}
+export default evennums;
+
