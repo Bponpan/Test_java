234c2e20f04e2d4ae2d098874b54d02080afdce3
ponpanB <151675683+Bponpan@users.noreply.github.com>
2024-01-30 12:02:59 +0700

    even number

---
+++ num.java
@@ -0,0 +1,12 @@
+public class num {
+    public static void main(String[] args) {
+
+        for (int i = 2; i <= 100; i += 2) {
+            System.out.print(i);
+
+            if (i < 100 - 1) {
+                System.out.print(",");
+            }
+        }
+    }
+}
